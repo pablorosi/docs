f1aac46b205fdeaf79ba8c6df6ac08cb77d064d8
Pablo Rosi <pablorosinavarro@gmail.com>
2026-04-29 23:57:15 +0200

    feat(ui): format home menu and scaffold docs structure

---
+++ astro.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			customCss: [
+				'@fontsource/jetbrains-mono',
+				'@fontsource/inter',
+				'./src/styles/custom.css'],
+			title: 'pablo@docs:~$',
+			
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' },
+				{ icon: 'linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/in/pablorosi/' },
+			],
+			sidebar: [
+				{
+					label: 'Secure Cloud Routing',
+					autogenerate: { directory: 'secure-cloud-routing' },
+				},
+				{
+					label: 'Enterprise Homelab',
+					autogenerate: { directory: 'enterprise-homelab' },
+				},
+				{
+					label: 'DB Administration',
+					autogenerate: { directory: 'db-administration' },
+				},
+			],
+		}),
+	],
+});
